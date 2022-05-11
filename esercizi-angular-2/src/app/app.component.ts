6ccf0ca5bd5d08b2294cd7af21e38d95c295178e
Giuseppe Marchesiello <juzeppemar90@gmail.com>
2022-05-11 15:16:44 +0200

    ex-12 completed

--- esercizi-angular-2/src/app/app.component.ts
+++ esercizi-angular-2/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CovidApiService } from './api/covid-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'esercizi-angular-2';
+export class AppComponent implements OnInit, OnDestroy {
+  subscription!: Subscription;
+  response: [string, string][] = [];
+
+  constructor(private api: CovidApiService) { }
+
+  ngOnInit(): void {
+    this.subscription = this.api.getResponse()
+      .subscribe((res: any) => {
+        Object.keys(res).forEach((element: string) => {
+          this.response.push([element, res[element].Path]);
+        });
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
 }

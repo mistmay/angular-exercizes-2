e663c08f6a0fa5432016576d2e93d0a877089d4a
Giuseppe Marchesiello <juzeppemar90@gmail.com>
2022-05-11 13:00:48 +0200

    ex-10 completed

--- esercizi-angular-2/src/app/components/show-counter.component.ts
+++ esercizi-angular-2/src/app/components/show-counter.component.ts
@@ -1,30 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CounterService } from '../services/counter.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-show-counter',
   template: `
     <div class="container p-5 bg-primary text-white fw-bold fs-3 text-center">
-      <h2>Counter Value : {{ getCounter() }}</h2>
-      <p class="text-white fw-bold mt-2 text-center" *ngIf="getErrorNotification()">Error: Counter cannot be below zero!!</p>
+      <h2>Counter Value : {{ counterNumber }}</h2>
+      <p class="text-white fw-bold mt-2 text-center" *ngIf="showError">Error: Counter cannot be below zero!!</p>
     </div>
   `,
   styles: [
   ]
 })
-export class ShowCounterComponent implements OnInit {
+export class ShowCounterComponent implements OnInit, OnDestroy {
+  counterNumber!: number;
+  showError!: boolean;
+  subscriptions: Subscription[] = [];
 
   constructor(private counter: CounterService) { }
 
   ngOnInit(): void {
+    this.subscriptions.push(
+      this.counter.getCounter()
+        .subscribe((res: number) => {
+          this.counterNumber = res;
+        })
+    );
+    this.subscriptions.push(
+      this.counter.getShowError()
+        .subscribe((res: boolean) => {
+          this.showError = res;
+        })
+    );
   }
 
-  getCounter(): number {
-    return this.counter.getCounter();
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription: Subscription) => {
+      subscription.unsubscribe();
+    });
   }
 
-  getErrorNotification(): boolean {
-    return this.counter.getShowError();
-  }
 
 }
--- esercizi-angular-2/src/app/services/counter.service.ts
+++ esercizi-angular-2/src/app/services/counter.service.ts
@@ -1,41 +1,45 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CounterService {
   private counter: number = 0;
-  private showError: boolean = false;
+  counterSubject: BehaviorSubject<number> = new BehaviorSubject<number>(this.counter);
+  errorSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   constructor() { }
 
-  getCounter(): number {
-    return this.counter;
+  getCounter(): Observable<number> {
+    return this.counterSubject.asObservable();
   }
 
-  getShowError(): boolean {
-    return this.showError;
+  getShowError(): Observable<boolean> {
+    return this.errorSubject.asObservable();
   }
 
   incrementCounter(number: number = 1): void {
     if (this.counter + number < 0) {
       console.log('Error: Counter cannot be below zero');
-      this.showError = true;
+      this.errorSubject.next(true);
     } else {
       this.counter += number;
+      this.counterSubject.next(this.counter);
       console.log(this.counter);
-      this.showError = false;
+      this.errorSubject.next(false);
     }
   }
 
   decreaseCounter(number: number = 1): void {
     if (this.counter - number < 0) {
       console.log('Error: Counter cannot be below zero');
-      this.showError = true;
+      this.errorSubject.next(true);
     } else {
       this.counter -= number;
+      this.counterSubject.next(this.counter);
       console.log(this.counter);
-      this.showError = false;
+      this.errorSubject.next(false);
     }
   }
 }
